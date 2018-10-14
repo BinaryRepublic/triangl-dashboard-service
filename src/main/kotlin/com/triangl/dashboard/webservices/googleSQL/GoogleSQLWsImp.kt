e8ff26f5d504ae73f249df30d7a857c0a2b227b5
Timon Christiansen <32591853+8BitJonny@users.noreply.github.com>
2018-10-14 23:29:55 +0200

    changed db fetch from createdAt to timestamp (#6)

--- src/main/kotlin/com/triangl/dashboard/webservices/googleSQL/GoogleSQLWsImp.kt
+++ src/main/kotlin/com/triangl/dashboard/webservices/googleSQL/GoogleSQLWsImp.kt
@@ -16,24 +16,23 @@ class GoogleSQLWsImp: GoogleSQLWs {
     var jdbcTemplate: JdbcTemplate? = null
 
     override fun countDistinctDeviceIdsInTimeFrame(customerId: String, start: String, end: String): Int {
-        println("SELECT COUNT(DISTINCT(trackedDeviceId)) FROM TrackingPoint WHERE createdAt > $start AND createdAt < $end")
-        return jdbcTemplate!!.queryForObject("SELECT COUNT(DISTINCT(trackedDeviceId)) FROM TrackingPoint WHERE createdAt > ? AND createdAt < ?", Long::class.java, start, end).toString().toInt()
+        return jdbcTemplate!!.queryForObject("SELECT COUNT(DISTINCT(trackedDeviceId)) FROM TrackingPoint WHERE timestamp > ? AND timestamp < ?", Long::class.java, start, end).toString().toInt()
     }
 
     override fun selectAllDeviceIdWithCoordinateInTimeframe(mapId: String, start: String, end: String): List<TrackingPointCoordinateJoin> {
-        return jdbcTemplate!!.query("SELECT TrackingPoint.trackedDeviceId, Coordinate.x, Coordinate.y, TrackingPoint.createdAt FROM TrackingPoint INNER JOIN Coordinate on TrackingPoint.coordinateId = Coordinate.id") {
+        return jdbcTemplate!!.query("SELECT TrackingPoint.trackedDeviceId, Coordinate.x, Coordinate.y, TrackingPoint.timestamp FROM TrackingPoint INNER JOIN Coordinate on TrackingPoint.coordinateId = Coordinate.id") {
             rs, _ -> TrackingPointCoordinateJoin().apply { trackedDeviceId = rs.getString("TrackingPoint.trackedDeviceId")
                                                            x = rs.getFloat("Coordinate.x")
                                                            y = rs.getFloat("Coordinate.y")
-                                                           createdAt = rs.getTimestamp("createdAt").toInstant().toString() }
+                                                           createdAt = rs.getTimestamp("timestamp").toInstant().toString() }
         }
     }
 
     override fun selectAllDeviceIdInTimeframe(mapId: String, start: String, end: String): List<TrackingPoint> {
-        return jdbcTemplate!!.query("SELECT id, trackedDeviceId, coordinateId, createdAt FROM TrackingPoint WHERE createdAt > ? AND createdAt < ?", arrayOf(start, end)) {
+        return jdbcTemplate!!.query("SELECT id, trackedDeviceId, coordinateId, createdAt FROM TrackingPoint WHERE timestamp > ? AND timestamp < ?", arrayOf(start, end)) {
             rs, _ -> TrackingPoint().apply { trackedDeviceId = rs.getString("trackedDeviceId")
                                     coordinateId = rs.getString("coordinateId")
-                                    createdAt = rs.getTimestamp("createdAt").toLocalDateTime() }
+                                    createdAt = rs.getTimestamp("timestamp").toLocalDateTime() }
         }
     }
 }
