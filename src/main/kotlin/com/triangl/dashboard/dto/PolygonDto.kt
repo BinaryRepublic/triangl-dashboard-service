47f2b55bd69b750ec841f2a5d3eb552dd90b60ad
8BitJonny <timon99@gmx.de>
2018-11-02 18:16:49 +0100

    CLEAN CODE :sunglasses:

--- src/main/kotlin/com/triangl/dashboard/dto/PolygonDto.kt
+++ src/main/kotlin/com/triangl/dashboard/dto/PolygonDto.kt
@@ -5,14 +5,10 @@ import java.awt.Polygon
 import kotlin.math.roundToInt
 
 class PolygonDto (
+    @JsonValue
     private val corners: List<LocationDto>
 ): Polygon(
     corners.map { it.x.roundToInt() }.toIntArray(),
     corners.map { it.y.roundToInt() }.toIntArray(),
     corners.size
-) {
-    @JsonValue
-    fun jsonRepresentation(): List<LocationDto>{
-        return corners
-    }
-}
+)
