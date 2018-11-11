2895fdded1451a9fa25090932f78e37b67963956
8BitJonny <timon99@gmx.de>
2018-11-11 21:54:09 +0100

    applied PR review suggestions

--- src/main/kotlin/com/triangl/dashboard/webservices/googleSQL/CustomCloudSqlJdbcInfoProvider.kt
+++ src/main/kotlin/com/triangl/dashboard/webservices/googleSQL/CustomCloudSqlJdbcInfoProvider.kt
@@ -1,13 +1,21 @@
 package com.triangl.dashboard.webservices.googleSQL
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.gcp.autoconfigure.sql.CloudSqlJdbcInfoProvider
 import org.springframework.context.annotation.Configuration
 
 
 @Configuration
 class CustomCloudSqlJdbcInfoProvider: CloudSqlJdbcInfoProvider {
+
+    @Value("\${spring.cloud.gcp.sql.database-name}")
+    val databaseName: String? = null
+
+    @Value("\${spring.cloud.gcp.sql.instance-connection-name}")
+    val instanceConnectionName: String? = null
+
     override fun getJdbcUrl(): String {
-        return "jdbc:mysql://google/serving-prod?useLegacyDatetimeCode=false&cloudSqlInstance=triangl-215714:europe-west3:analyzing&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false"
+        return "jdbc:mysql://google/$databaseName?useLegacyDatetimeCode=false&cloudSqlInstance=$instanceConnectionName&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false"
     }
 
     override fun getJdbcDriverClass(): String {
