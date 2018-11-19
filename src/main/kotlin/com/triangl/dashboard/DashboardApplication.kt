80f065c92ed1db8cc56865a907bbd9b461866356
Tobias Trinks <33936430+tobiastrinks@users.noreply.github.com>
2018-11-19 14:04:12 +0100

    Feat add oauth2 (#19)
    * Add jwt token verification facade
    
    * Disable oauth2 for integration tests
    
    * Remove redundant TokenAuthentication class
    
    * Use implementation instead of compile for gradle dependencies
    
    * Handle cross origin in OAuthConfig now

--- src/main/kotlin/com/triangl/dashboard/DashboardApplication.kt
+++ src/main/kotlin/com/triangl/dashboard/DashboardApplication.kt
@@ -2,8 +2,11 @@ package com.triangl.dashboard
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.annotation.PropertySources
 
 @SpringBootApplication
+@PropertySources(PropertySource("classpath:application.properties"), PropertySource("classpath:auth0.properties"))
 class DashboardApplication
 
 fun main(args: Array<String>) {
